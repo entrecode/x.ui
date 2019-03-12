2b404d0cc7297914f851af1f900b3d863bf770d4
Bernhard Ruoff <ruoff@entrecode.de>
2019-03-12 14:48:16 +0100

    ducumentation

--- src/components/Grid/Grid.vue
+++ src/components/Grid/Grid.vue
@@ -1,11 +1,11 @@
 <template>
-  <section id="scroll-to-grif">
+  <section id="scroll-to-grid">
     <h2 class="align-center">Grid</h2>
-    <a href @click.prevent="addCol">add</a>
+    <a href @click.prevent="addCol"><svg class="ixo"><use xlink:href="#add"></use></svg></a>
 
-    <div :data-grid="gridAlign" class>
+    <div :data-grid="[gridAlign + ' ' + gridGutterSize]" class>
       <div data-col="4" v-for="(col, index) in cols" :key="index">
-        <div class="is-giga is-theme-grid align-center has-padding-3">{{col}}</div>
+        <div class="is-giga is-theme-grid align-center has-padding-3 has-margin-bottom-3">{{col}}</div>
       </div>
     </div>
   </section>
@@ -18,11 +18,12 @@ export default {
     return {
       cols: ['col', 'col'],
       gridAlign: 'is-center',
+      gridGutterSize: 'is-small',
     };
   },
   methods: {
     addCol: function() {
-      console.log('hihi')
+      console.log('hihi');
       this.cols.push('col');
     },
   },
@@ -30,6 +31,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-[data-grid] {
-}
 </style>
