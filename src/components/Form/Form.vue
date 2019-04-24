cd8ba744ebb7217ada6eb219be7b3dff6e6bbd8e
Bernhard Ruoff <ruoff@entrecode.de>
2019-04-24 15:12:15 +0200

    fixed xui-toggle

--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -215,29 +215,59 @@ $xui-radio-dot-checked-color: $link;`,
               title: 'toggle',
               preview: `<div data-grid>
 <div data-col='6'>
-<div class="xui-toggle">
+<div class="field-group">
+  <div class="xui-toggle">
   <input id="toggle-1" type="checkbox">
-  <label for="toggle-1">
+  <label for="toggle-1" class="xui-toggle__label">
     <div class="xui-toggle__switch"></div>
     toggle with a longer label
   </label>
 </div>
 </div>
+</div>
 <div data-col='6'>
+<div class="field-group">
 <div class="xui-toggle">
   <input id="toggle-2" type="checkbox" checked>
-  <label for="toggle-2">
+  <label for="toggle-2" class="xui-toggle__label">
     <div class="xui-toggle__switch"></div>
     toggle
   </label>
 </div>
 </div>
+</div>
+<div data-col='6'>
+<div class="field-group">
+<div class="xui-toggle">
+  <input id="toggle-3" type="checkbox">
+  <label for="toggle-3" class="xui-toggle__switch">
+  </label>
+</div>
+</div>
+</div>
+<div data-col='6'>
+<div class="field-group">
+<div class="xui-toggle">
+  <input id="toggle-4" type="checkbox" checked>
+  <label for="toggle-4" class="xui-toggle__switch">
+  </label>
+</div>
+</div>
+</div>
 </div>`,
-              markup: `<div class="xui-toggle">
+              markup: `<!-- switch with a label -->
+<div class="xui-toggle">
   <input id="" type="checkbox">
-  <label for="">
+  <label for="" class="xui-toggle__label">
     <div class="xui-toggle__switch"></div>
     toggle with a longer label
+  </label>
+</div>
+
+<!-- just the switch -->
+<div class="xui-toggle">
+  <input id="" type="checkbox">
+  <label for="" class="xui-toggle__switch">
   </label>
 </div>`,
               config: `$xui-toggle-background: $background-lighter;
@@ -255,8 +285,9 @@ $xui-toggle-pin-width: $xui-toggle-pin-height;
 $xui-toggle-pin-radius: $xui-toggle-pin-height / 2;
 $xui-toggle-pin-style: ();
 
-$xui-toggle-label-size: $base-font-size;
-$xui-toggle-line-height: $xui-toggle-label-size * $base-line-height;`,
+$xui-toggle-label-font-size: $base-font-size;
+$xui-toggle-label-line-height: $xui-toggle-label-font-size * $base-line-height;
+$xui-toggle-labelstyle: ();`,
             },
           ],
         },
