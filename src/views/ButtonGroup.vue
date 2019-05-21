4bc5702179e81764f6a9862e4f17437204341f7d
Bernhard Ruoff <ruoff@entrecode.de>
2019-05-21 14:03:27 +0200

    btn group doc

--- src/views/ButtonGroup.vue
+++ src/views/ButtonGroup.vue
@@ -1,53 +1,89 @@
 <template>
-  <blocks-group :groups="sectionGroups" section-title="btn-group"></blocks-group>
+  <section>
+    <a
+      @click="activeTab = 1"
+      class="btn btn_clear demo-tab"
+      :class="{'is-active' : activeTab === 1}"
+    >normal</a>
+    <a
+      @click="activeTab = 2"
+      class="btn btn_clear demo-tab"
+      :class="{'is-active' : activeTab === 2}"
+    >rounded</a>
+    <a
+      @click="activeTab = 3"
+      class="btn btn_clear demo-tab"
+      :class="{'is-active' : activeTab === 3}"
+    >dropdown</a>
+    <div v-if="activeTab === 1">
+      <div class="demo-preview has-padding-8" data-flex="center center-items">
+        <div class="btn-group">
+          <a class="btn-group__start btn">button</a>
+          <div class="btn-group__divider"></div>
+          <a class="btn-group__middle btn">button</a>
+          <div class="btn-group__divider"></div>
+          <a class="btn-group__end btn">button</a>
+        </div>
+      </div>
+      <pre v-highlightjs="markup1"><code class="html"></code></pre>
+    </div>
+    <div v-if="activeTab === 2">
+      <div class="demo-preview has-padding-8" data-flex="center center-items">
+        <div class="btn-group">
+          <a class="btn-group__start btn btn_round">button</a>
+          <a class="btn-group__end btn btn_super btn_round">button</a>
+        </div>
+      </div>
+      <pre v-highlightjs="markup2"><code class="html"></code></pre>
+    </div>
+    <div v-if="activeTab === 3">
+      <div class="demo-preview has-padding-8" data-flex="center center-items">
+        <div class="btn-group">
+          <a class="btn-group__start btn_outlined btn">button</a>
+          <div class="btn-group__divider"></div>
+          <div class="dropdown" tabindex="0">
+            <div class="btn btn_outlined btn-group__end">
+              <svg class="ixo">
+                <use xlink:href="#more-vertical"></use>
+              </svg>
+            </div>
+            <div class="dropdown-options">...</div>
+          </div>
+        </div>
+      </div>
+      <pre v-highlightjs="markup3"><code class="html"></code></pre>
+    </div>
+    <div data-col="8@sm">
+      <params-table :params="params"></params-table>
+      <requires-list :requires="requires"></requires-list>
+    </div>
+  </section>
 </template>
 
 <script>
-import BlocksGroup from '@/components/BlocksGroup.vue';
-
+import ParamsTable from '@/components/ParamsTable.vue';
+import RequiresList from '@/components/RequiresList.vue';
 export default {
   name: 'ButtonGroup',
   components: {
-    BlocksGroup,
+    ParamsTable,
+    RequiresList,
   },
   data: () => {
     return {
-      sectionGroups: [
-        {
-          title: '',
-          id: 'btn-group',
-          navTitle: 'Button-Group',
-          summary: '',
-          groupItems: [
-            {
-              title: 'btn-group',
-              preview: `<div class="btn-group">
+      activeTab: 1,
+      markup1: `<div class="btn-group">
   <a class="btn-group__start btn">button</a>
   <div class="btn-group__divider"></div>
-  <a class="btn-group__end btn btn_super">button</a>
-</div>
-<div class="y-space-2"></div>
-<div class="btn-group">
+  <a class="btn-group__middle btn ">button</a>
+  <div class="btn-group__divider"></div>
+  <a class="btn-group__end btn ">button</a>
+</div>`,
+      markup2: `<div class="btn-group">
   <a class="btn-group__start btn btn_round">button</a>
   <a class="btn-group__end btn btn_super btn_round">button</a>
 </div>`,
-              config: `$btn-group-style: ();
-$btn-group-divider-size: 1;
-$btn-group-divider-color: $base-divider-color`,
-            },
-            {
-              title: 'btn-group with a dropdown',
-              preview: `<div class="btn-group">
-  <a class="btn-group__start btn_outlined btn">button</a>
-  <div class="btn-group__divider"></div>
-  <div class="dropdown" tabindex="0">
-    <div class="btn  btn_outlined btn-group__end"><svg class="ixo"><use xlink:href="#more-vertical"></use></svg></div>
-    <div class="dropdown-options">
-      ...
-    </div>
-  </div>
-</div>`,
-              markup: `<div class="btn-group">
+      markup3: `<div class="btn-group">
   <a class="btn-group__start btn">button</a>
   <div class="btn-group__divider"></div>
   <div class="dropdown" tabindex="0">
@@ -57,11 +93,23 @@ $btn-group-divider-color: $base-divider-color`,
     </div>
   </div>
 </div>`,
-              config: ``,
-            },
-          ],
+      params: [
+        {
+          name: '$btn-group-divider-size',
+          type: 'number',
+          default: '1',
+        },
+        {
+          name: '$btn-group-divider-color',
+          type: 'color',
+          default: '$base-divider-color // rgba($text, 0.1)',
+        },
+        {
+          name: '$btn-group-style',
+          type: 'map',
         },
       ],
+      requires: [{ name: 'rem()', type: 'function', link: '#scroll-to-rem' }],
     };
   },
 };
