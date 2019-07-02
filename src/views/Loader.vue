e58f5bfcc4a7625e2401aefb9190edb7758e1555
Bernhard Ruoff <ruoff@entrecode.de>
2019-07-02 17:24:20 +0200

    code style

--- src/views/Loader.vue
+++ src/views/Loader.vue
@@ -1,52 +1,82 @@
 <template>
   <section>
-    <div class="nav">
-      <div class="nav__item">
-        <a @click="activeTab = 1" class="btn btn_clear demo-tab" :class="{ 'is-active': activeTab === 1 }">inline</a>
-      </div>
-      <div class="nav__item">
-        <a @click="activeTab = 2" class="btn btn_clear demo-tab" :class="{ 'is-active': activeTab === 2 }">local</a>
-      </div>
-      <div class="nav__item">
-        <a @click="activeTab = 3" class="btn btn_clear demo-tab" :class="{ 'is-active': activeTab === 3 }">global</a>
-      </div>
-    </div>
-
-    <div v-if="activeTab === 1" class="animated fadeIn">
-      <div class="demo-preview is-padding-8 is-margin-bottom-5">
-        <div data-grid>
-          <div data-col="4" data-flex="center center-items"><span class="loader"></span></div>
-          <div data-col="4" data-flex="center center-items">
-            <a class="btn"> <span class="loader"></span> </a>
+    <div data-grid>
+      <div data-col="10 8@xl">
+        <div class="is-padding-center-3">
+          <h2>loader</h2>
+        </div>
+        <div class="nav">
+          <div class="nav__item">
+            <a
+              @click="activeTab = 1"
+              class="btn btn_clear demo-tab"
+              :class="{ 'is-active': activeTab === 1 }"
+            >inline</a>
           </div>
-          <div data-col="4" data-flex="center center-items">
-            <div class="input-group">
-              <input type="text" class="input input_big" placeholder="input..." />
-              <div class="input-group__addon"><span class="loader"></span></div>
+          <div class="nav__item">
+            <a
+              @click="activeTab = 2"
+              class="btn btn_clear demo-tab"
+              :class="{ 'is-active': activeTab === 2 }"
+            >local</a>
+          </div>
+          <div class="nav__item">
+            <a
+              @click="activeTab = 3"
+              class="btn btn_clear demo-tab"
+              :class="{ 'is-active': activeTab === 3 }"
+            >global</a>
+          </div>
+        </div>
+
+        <div v-if="activeTab === 1" class="animated fadeIn">
+          <div class="demo-preview is-padding-8 is-margin-bottom-5">
+            <div data-grid>
+              <div data-col="4" data-flex="center center-items">
+                <span class="loader"></span>
+              </div>
+              <div data-col="4" data-flex="center center-items">
+                <a class="btn">
+                  <span class="loader"></span>
+                </a>
+              </div>
+              <div data-col="4" data-flex="center center-items">
+                <div class="input-group">
+                  <input type="text" class="input input_big" placeholder="input..." />
+                  <div class="input-group__addon">
+                    <span class="loader"></span>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
+          <pre v-highlightjs="markup1"><code class="html"></code></pre>
+        </div>
+        <div v-if="activeTab === 2" class="animated fadeIn">
+          <div class="demo-preview is-padding-8 is-margin-bottom-5" data-flex="center center-items">
+            <a class="btn btn_super is-relative">
+              save
+              <span class="loader is-local"></span>
+            </a>
+          </div>
+          <pre v-highlightjs="markupLocal"><code class="html"></code></pre>
+        </div>
+        <div v-if="activeTab === 3" class="animated">
+          <div class="demo-preview is-padding-8 is-margin-bottom-5" data-flex="center center-items">
+            <a class="btn" @click="showLoader = !showLoader">show global loader</a>
+            <div class="loader is-global" v-if="showLoader" @click="showLoader = !showLoader"></div>
+          </div>
+          <pre v-highlightjs="markupGlobal"><code class="html"></code></pre>
+        </div>
+        <div data-grid="center">
+          <div data-col="10@md 8@lg">
+            <div class="spacer"></div>
+            <params-table :params="params"></params-table>
+            <div class="divider"></div>
+            <requires-list :requires="requires"></requires-list>
           </div>
         </div>
       </div>
-      <pre v-highlightjs="markup1"><code class="html"></code></pre>
-    </div>
-    <div v-if="activeTab === 2" class="animated fadeIn">
-      <div class="demo-preview is-padding-8 is-margin-bottom-5" data-flex="center center-items">
-        <a class="btn btn_super is-relative"> save <span class="loader is-local"></span> </a>
-      </div>
-      <pre v-highlightjs="markupLocal"><code class="html"></code></pre>
-    </div>
-    <div v-if="activeTab === 3" class="animated">
-      <div class="demo-preview is-padding-8 is-margin-bottom-5" data-flex="center center-items">
-        <a class="btn" @click="showLoader = !showLoader">show global loader</a>
-        <div class="loader is-global" v-if="showLoader" @click="showLoader = !showLoader"></div>
-      </div>
-      <pre v-highlightjs="markupGlobal"><code class="html"></code></pre>
-    </div>
-    <div data-col="10@md 8@lg">
-      <div class="y-space-8"></div>
-      <params-table :params="params"></params-table>
-      <div class="divider"></div>
-      <requires-list :requires="requires"></requires-list>
     </div>
   </section>
 </template>
