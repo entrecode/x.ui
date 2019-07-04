920acc9e1b4677fc08730e67c02ae2b2bcb60a7b
Bernhard Ruoff <ruoff@entrecode.de>
2019-07-04 16:36:06 +0200

    accordion demo

--- src/views/interface/XuiAccordion.vue
+++ src/views/interface/XuiAccordion.vue
@@ -1,5 +1,84 @@
 <template>
-  <blocks-group :groups="sectionGroups" section-title="xui-accordion"></blocks-group>
+  <section>
+    <div data-col="10 8@xl">
+      <div class="titlebar">
+        <h1>xui-accordion</h1>
+      </div>
+      <div class="nav">
+        <div class="nav__item">
+          <a
+            @click="activeTab = 1"
+            class="btn btn_clear demo-tab"
+            :class="{ 'is-active': activeTab === 1 }"
+          >single</a>
+        </div>
+        <div class="nav__item">
+          <a
+            @click="activeTab = 2"
+            class="btn btn_clear demo-tab"
+            :class="{ 'is-active': activeTab === 2 }"
+          >multiple</a>
+        </div>
+        <div class="nav__item">
+          <a
+            @click="activeTab = 3"
+            class="btn btn_clear demo-tab"
+            :class="{ 'is-active': activeTab === 3 }"
+          >with handle</a>
+        </div>
+        <div class="nav__item">
+          <a
+            @click="activeTab = 4"
+            class="btn btn_clear demo-tab"
+            :class="{ 'is-active': activeTab === 4 }"
+          >combined</a>
+        </div>
+        <div class="nav__item">
+          <a
+            @click="activeTab = 5"
+            class="btn btn_clear demo-tab"
+            :class="{ 'is-active': activeTab === 5 }"
+          >combined</a>
+        </div>
+      </div>
+      <div v-show="activeTab === 1">
+        <div class="demo-preview is-padding-8" data-flex="center">
+          <div class="is-padding-center-5 is-radius" data-col="10" v-html="previewBasic"></div>
+        </div>
+        <div class="spacer"></div>
+        <pre v-highlightjs="previewBasic"><code class="html"></code></pre>
+      </div>
+      <div v-show="activeTab === 2">
+        <div class="demo-preview is-padding-8" data-flex="center">
+          <div class="is-padding-center-5 is-radius" data-col="10" v-html="multiplePreview"></div>
+        </div>
+        <div class="spacer"></div>
+        <pre v-highlightjs="multiplePreview"><code class="html"></code></pre>
+      </div>
+      <div v-show="activeTab === 3">
+        <div class="demo-preview is-padding-8" data-flex="center">
+          <div class="is-padding-center-5 is-radius" data-col="10" v-html="handlePreview"></div>
+        </div>
+        <div class="spacer"></div>
+        <pre v-highlightjs="handlePreview"><code class="html"></code></pre>
+      </div>
+      <div v-show="activeTab === 4">
+        <div class="demo-preview is-padding-8" data-flex="center">
+          <div class="is-padding-center-5 is-radius" data-col="10" v-html="combinedPreview"></div>
+        </div>
+        <div class="spacer"></div>
+        <pre v-highlightjs="combinedPreview"><code class="html"></code></pre>
+      </div>
+      <div v-show="activeTab === 5">
+        <div class="demo-preview is-padding-8" data-flex="center">
+          <div class="is-padding-center-5 is-radius" data-col="10" v-html="combinedPreviewAlt"></div>
+        </div>
+        <div class="spacer"></div>
+        <pre v-highlightjs="combinedPreviewAlt"><code class="html"></code></pre>
+      </div>
+    </div>
+    <blocks-group :groups="sectionGroups" section-title="xui-accordion"></blocks-group>
+  </section>
 </template>
 
 <script>
@@ -12,14 +91,8 @@ export default {
   },
   data: () => {
     return {
-      sectionGroups: [
-        {
-          title: 'toggle single',
-          id: 'xui-accordion-single-toggle',
-          navTitle: 'single toggle',
-          groupItems: [
-            {
-              preview: `<div class="xui-accordion">
+      activeTab: 1,
+      previewBasic: `<div class="xui-accordion">
   <input
     type="radio"
     name="xuiToggleGroup"
@@ -27,92 +100,62 @@ export default {
     checked="checked"
   />
   <label for="toggleList1" class="xui-accordion__handle">
-    Entry 1
+    click to toggle
   </label>
   <div class="xui-accordion__target">
-    Content 1
+    <strong>here i am</strong>
   </div>
 </div>
 <div class="xui-accordion">
   <input type="radio" name="xuiToggleGroup" id="toggleList2" />
   <label for="toggleList2" class="xui-accordion__handle">
-    Entry 2
+    click to toggle
   </label>
   <div class="xui-accordion__target">
-    Content 2
+    <strong>here i am</strong>
   </div>
 </div>
 <div class="xui-accordion">
   <input type="radio" name="xuiToggleGroup" id="toggleList3" />
   <label for="toggleList3" class="xui-accordion__handle">
-    Entry 3
+    click to toggle
   </label>
   <div class="xui-accordion__target">
-    Content 3
+    <strong>here i am</strong>
   </div>
 </div>`,
-              markup: `<div class="xui-accordion">
-  <input type="radio" name="" id="" checked="checked" />
-  <label for="" class="xui-accordion__handle">...</label>
-  <div class="xui-accordion__target">...</div>
-</div>
-`,
-            },
-          ],
-        },
-        {
-          title: 'toggle multiple',
-          id: 'xui-accordion-multiple-toggle',
-          navTitle: 'multiple toggle',
-          groupItems: [
-            {
-              preview: `<div class="xui-accordion">
+      multiplePreview: `<div class="xui-accordion">
   <input
     type="checkbox"
     id="toggleListMulti1"
     checked="checked"
   />
   <label for="toggleListMulti1" class="xui-accordion__handle">
-    Entry 1
+    click to toggle
   </label>
   <div class="xui-accordion__target">
-    Content 1
+    <strong>here i am</strong>
   </div>
 </div>
 <div class="xui-accordion">
   <input type="checkbox" id="toggleListMulti2" />
   <label for="toggleListMulti2" class="xui-accordion__handle">
-    Entry 2
+    click to toggle
   </label>
   <div class="xui-accordion__target">
-    Content 2
+    <strong>here i am</strong>
   </div>
 </div>
 <div class="xui-accordion">
   <input type="checkbox" id="toggleListMulti3" />
   <label for="toggleListMulti3" class="xui-accordion__handle">
-    Entry 3
+    click to toggle
   </label>
   <div class="xui-accordion__target">
-    Content 3
+    <strong>here i am</strong>
   </div>
 </div>`,
-              markup: `<div class="xui-accordion">
-  <input type="checkbox" name="" id="" checked="checked" />
-  <label for="" class="xui-accordion__handle">...</label>
-  <div class="xui-accordion__target">...</div>
-</div>
-`,
-            },
-          ],
-        },
-        {
-          title: 'advanced',
-          id: 'xui-accordion-advanced',
-          navTitle: 'examples',
-          groupItems: [
-            {
-              preview: `<div class="xui-accordion is-border-bottom is-padding-3">
+      handlePreview: `<div class="xui-accordion is-padding-3">
   <input
     type="checkbox"
     id="toggleListAdvanced1"
@@ -130,23 +173,8 @@ export default {
     Effective altruism collaborate social return on investment social enterprise inclusion sustainable. 
   </div>
 </div>
-<div class="xui-accordion is-border-bottom is-padding-3">
-  <input
-    type="checkbox"
-    id="toggleListAdvanced2"
-  />
-  <label for="toggleListAdvanced2" data-flex="center-items" class="xui-accordion__handle">
-    <a class="is-mega">Effective altruism</a>
-    <div class="flex-space"></div>
-    <div class="btn btn_clear">
-      <svg class="ixo is-visible-collapsed"><use xlink:href="#expand-horizontal"></use></svg>
-      <svg class="ixo is-hidden-collapsed"><use xlink:href="#shrink-horizontal"></use></svg>
-    </div>
-  </label>
-  <div class="xui-accordion__target">
-    Effective altruism collaborate social return on investment social enterprise inclusion sustainable. 
-  </div>
-</div>
+`,
+      combinedPreview: `
 <div class="xui-accordion is-padding-3">
   <input
     type="checkbox"
@@ -171,7 +199,8 @@ export default {
   <div class="xui-accordion__target">
     Effective altruism collaborate social return on investment social enterprise inclusion sustainable. 
   </div>
-</div>
+</div>`,
+      combinedPreviewAlt: `
 <div class="xui-accordion card is-padding-2">
   <input
     type="checkbox"
@@ -189,10 +218,6 @@ export default {
     </div>
   </label>
 </div>`,
-            },
-          ],
-        },
-      ],
     };
   },
 };
