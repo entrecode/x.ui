13e9847dcf1be65d195c7ef8a86dce9a8134edbf
Bernhard Ruoff <ruoff@entrecode.de>
2019-07-04 14:45:02 +0200

    card doc

--- src/views/ui/Card.vue
+++ src/views/ui/Card.vue
@@ -1,40 +1,79 @@
 <template>
-  <blocks-group :groups="sectionGroups" section-title="card"></blocks-group>
+  <section>
+    <div class="is-padding-center-3">
+      <h2>card</h2>
+    </div>
+    <div data-col="10@md 8@lg">
+      <div class="demo-preview is-padding-8">
+        <div class="card">
+          <h2>catalyze design thinking</h2>
+          <p>academic thought leadership. Emerging innovate; society humanitarian change-makers benefit corporation.</p>
+        </div>
+      </div>
+      <div class="spacer"></div>
+      <pre v-highlightjs="markup"><code class="html"></code></pre>
+      <div class="spacer"></div>
+      <params-table :params="params"></params-table>
+      <div class="spacer"></div>
+      <requires-list :requires="requires"></requires-list>
+    </div>
+  </section>
 </template>
 
 <script>
-import BlocksGroup from '@/components/BlocksGroup.vue';
+import ParamsTable from '@/components/ParamsTable.vue';
+import RequiresList from '@/components/RequiresList.vue';
 
 export default {
   components: {
-    BlocksGroup,
+    ParamsTable,
+    RequiresList,
   },
   data: () => {
     return {
-      sectionGroups: [
-        {
-          title: '',
-          id: 'card',
-          navTitle: 'Default Card',
-          groupItems: [
-            {
-              title: 'Default Card',
-              preview: `<div class="card">
-  <h2>Titel</h2>
-  <p>card is a standard ui element to combine related Informations or elements</p>
-</div>`,
-              markup: `<div class="card">
+      markup: `<div class="card">
   ...
 </div>`,
-              config: `@param {number} $card-radius [4]
-@param {color} $card-background [$background]
-@param {color} $card-color [$text]
-@param {array} $card-padding [24]
-@param {array} $card-spacing [0]
-@param {[1-24] | false} $card-elevated [4]
-@param {array} $card-style`,
-            },
-          ],
+      requires: [
+        { name: 'array-magic()', type: 'function', link: '#scroll-to-array-magic' },
+        { name: 'inject-style()', type: 'mixin', link: '#scroll-to-inject-style' },
+        { name: 'elevation()', type: 'mixin', link: '#scroll-to-elevation' },
+      ],
+      params: [
+        {
+          name: '$card-background',
+          type: 'color',
+          default: '$background',
+        },
+        {
+          name: '$card-color',
+          type: 'color',
+          default: '$text',
+        },
+        {
+          name: '$card-padding',
+          type: 'array',
+          default: '24',
+        },
+        {
+          name: '$card-spacing',
+          type: 'array',
+          default: '0',
+        },
+        {
+          name: '$card-elevation',
+          type: '[1-24] | false',
+          default: '4',
+        },
+        {
+          name: '$card-radius',
+          type: 'number',
+          default: '4',
+        },
+        {
+          name: '$card-style',
+          type: 'array',
+          default: '',
         },
       ],
     };
