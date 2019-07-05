8a89e93bd6265bafbb8b4f4f4404a4064d3125a2
Bernhard Ruoff <ruoff@entrecode.de>
2019-07-05 16:20:00 +0200

    is-radius doc

--- src/views/style/IsRadius.vue
+++ src/views/style/IsRadius.vue
@@ -1,96 +1,123 @@
 <template>
-  <section class="demo-section" id="scroll-to-is-radius">
-    <div data-container>
-      <div data-grid>
-        <div data-col="10">
-          <doc-tab>
-            <div slot="preview">
-              <div class="demo-bg is-padding-6" :class="hasRadiusClass">
-                <div
-                  class="demo-radius-settings is-theme is-padding-center-6 is-padding-middle-3"
-                  :class="hasRadiusClass"
-                >
-                  <div>
-                    <div class="xui-checkbox">
-                      <input type="checkbox" id="radiusTL" v-model="radiusEdge.tl" />
-                      <label for="radiusTL" class="xui-checkbox__label"></label>
-                    </div>
-                  </div>
-                  <div></div>
-                  <div data-flex="end">
-                    <div class="xui-checkbox">
-                      <input type="checkbox" id="radiusTR" v-model="radiusEdge.tr" />
-                      <label for="radiusTR" class="xui-checkbox__label"></label>
-                    </div>
-                  </div>
-                  <div></div>
-                  <div>
-                    <select name id class="input" v-model="radiusOption">
-                      <option
-                        :value="option"
-                        v-for="(option, index) in radiusOptions"
-                        :key="index"
-                        v-text="option"
-                      ></option>
-                    </select>
-                  </div>
-                  <div></div>
-                  <div>
-                    <div class="xui-checkbox">
-                      <input type="checkbox" id="radiusBL" v-model="radiusEdge.bl" />
-                      <label for="radiusBL" class="xui-checkbox__label"></label>
-                    </div>
-                  </div>
-                  <div></div>
-                  <div data-flex="end">
-                    <div class="xui-checkbox">
-                      <input type="checkbox" id="radiusBR" v-model="radiusEdge.br" />
-                      <label for="radiusBR" class="xui-checkbox__label"></label>
-                    </div>
-                  </div>
-                </div>
+  <section>
+    <div data-col="10 8@xl">
+      <div class="titlebar">
+        <h1>is-radius</h1>
+      </div>
+      <div class="demo-preview is-padding-8" data-flex="center">
+        <div class="demo-bg is-padding-6" :class="isRadiusClass">
+          <div
+            class="demo-radius-settings is-theme is-padding-center-6 is-padding-middle-3"
+            :class="isRadiusClass"
+          >
+            <div>
+              <div class="xui-checkbox">
+                <input type="checkbox" id="radiusTL" v-model="tl" />
+                <label for="radiusTL" class="xui-checkbox__label"></label>
               </div>
             </div>
-            <div slot="markup">
-              <pre v-highlightjs="markup"><code class="html"></code></pre>
+            <div></div>
+            <div data-flex="end">
+              <div class="xui-checkbox">
+                <input type="checkbox" id="radiusTR" v-model="tr" />
+                <label for="radiusTR" class="xui-checkbox__label"></label>
+              </div>
             </div>
-            <div slot="config">
-              <pre v-highlightjs="config"><code class="scss"></code></pre>
+            <div></div>
+            <div>
+              <select name id class="input" v-model="radiusOption">
+                <option
+                  :value="option"
+                  v-for="(option, index) in radiusOptions"
+                  :key="index"
+                  v-text="option"
+                ></option>
+              </select>
             </div>
-          </doc-tab>
-        </div>
-        <div data-col="2">
-          <ul class="nav nav_stacked is-sticky" style="top: 120px;">
-            <li class="nav__item"><h2>is-radius</h2></li>
-          </ul>
+            <div></div>
+            <div>
+              <div class="xui-checkbox">
+                <input type="checkbox" id="radiusBL" v-model="bl" />
+                <label for="radiusBL" class="xui-checkbox__label"></label>
+              </div>
+            </div>
+            <div></div>
+            <div data-flex="end">
+              <div class="xui-checkbox">
+                <input type="checkbox" id="radiusBR" v-model="br" />
+                <label for="radiusBR" class="xui-checkbox__label"></label>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <div class="spacer"></div>
+      <pre v-highlightjs="markup"><code class="html"></code></pre>
+      <div class="spacer"></div>
+      <params-table :params="params"></params-table>
     </div>
   </section>
 </template>
 
 <script>
-import DocTab from '@/components/DocTab.vue';
+import ParamsTable from '@/components/ParamsTable.vue';
+import RequiresList from '@/components/RequiresList.vue';
 
 export default {
   name: 'HasRadius',
   components: {
-    DocTab,
+    ParamsTable,
+    RequiresList,
   },
   data: () => {
     return {
-      radiusOptions: ['none', 'default', 'round'],
-      radiusOption: 'round',
-      radiusEdge: { tl: true, tr: true, bl: true, br: true },
-      hasRadiusClass: 'is-round',
-      markup: `<div class="is-radius-0"></div>
-<div class="is-radius"></div>
-<div class="is-round"></div>`,
-      config: `$is-radius-options: (
+      radiusOptions: ['is-radius-0', 'is-radius', 'is-round'],
+      radiusOption: 'is-radius',
+      tl: true,
+      tr: true,
+      br: true,
+      bl: true,
+      isRadiusClass: 'is-radius',
+      markup: `<div class="is-radius"></div>`,
+      params: [
+        {
+          name: '$is-radius-options',
+          type: 'map',
+          default: `(
   '0': 0,
-  '': 4,
-);`,
+  '': 8,
+)`,
+        },
+      ],
     };
+  },
+  computed: {
+    edges() {
+      const value = ['tl', 'tr', 'br', 'bl'].filter((v) => this[v]).join('');
+      const classes = {
+        tltr: ['-top'],
+        tlbl: ['-left'],
+        trbr: ['-right'],
+        brbl: ['-bottom'],
+        tltrbr: ['-top', '-br'],
+        tltrbl: ['-top', '-bl'],
+        tlbrbl: ['-bottom', '-tl'],
+        trbrbl: ['-bottom', '-tr'],
+        tltrbrbl: [''],
+      };
+      console.log(value);
+      return classes[value];
+    },
+    dirty() {
+      const d = ['radiusOption', 'tl', 'tr', 'br', 'bl'];
+      return d.map((e) => this[e]).join();
+    },
+  },
+  watch: {
+    dirty: function(val) {
+      this.isRadiusClass = this.edges.map((e) => this.radiusOption + e).join(' ');
+      this.markup = `<div class="${this.isRadiusClass}"></div>`;
+    },
   },
 };
 </script>
