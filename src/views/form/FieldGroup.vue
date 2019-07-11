6d0bc0d86ce5db423d5752485a2fcefd1ee33cf8
Bernhard Ruoff <ruoff@entrecode.de>
2019-07-11 10:26:19 +0200

    fix: field-group_boolean style

--- src/views/form/FieldGroup.vue
+++ src/views/form/FieldGroup.vue
@@ -22,17 +22,11 @@
       </div>
 
       <div class="demo-preview is-padding-8" data-flex="center" v-if="activeTab === 1">
-        <div class="is-theme is-padding-5 is-radius is-elevated-24" data-col="8@lg 6@xl">
-          <div class="field-group">
-            <label for class="field-group__label">field-group label</label>
-            <input type="text" placeholder="input..." class="input" />
-            <p class="field-group__info">field-group info</p>
-          </div>
-          <div class="field-group">
-            <input type="checkbox" id="3gjh3" />
-            <label for="3gjh3">field-group label</label>
-          </div>
-        </div>
+        <div
+          class="is-theme is-padding-5 is-radius is-elevated-24"
+          data-col="8@lg 6@xl"
+          v-html="defaultMarkup"
+        ></div>
       </div>
 
       <div class="demo-preview is-padding-8" data-flex="column center-items" v-if="activeTab === 2">
@@ -70,6 +64,10 @@
       <pre v-highlightjs="withGridMarkup" v-show="activeTab === 2"><code class="html"></code></pre>
       <div class="spacer"></div>
       <params-table :params="inputParams"></params-table>
+      <div class="divider"></div>
+      <params-table :params="labelParams" title="label params"></params-table>
+      <div class="divider"></div>
+      <params-table :params="infoParams" title="info params"></params-table>
       <div class="spacer"></div>
       <requires-list :requires="requires"></requires-list>
     </div>
@@ -95,13 +93,33 @@ export default {
       ],
       activeTab: 1,
       defaultMarkup: `<div class="field-group">
-  <label for="" class="field-group__label">field-group label</label>
-  <input type="text" placeholder="input..." class="input">
+  <label for class="field-group__label">field-group label</label>
+  <input type="text" placeholder="input..." class="input" />
   <p class="field-group__info">field-group info</p>
 </div>
+
+
+<div class="field-group field-group_boolean">
+  <label for="3gjh3" class="field-group__label">field-group label</label>
+  <input type="checkbox" id="3gjh3" />
+</div>
+
+<!-- boolean with infotext -->
 <div class="field-group">
-  <input type="checkbox" id="3gjh3">
-  <label for="3gjh3">field-group label</label>
+  <div class="field-group_boolean">
+    <label for="3gjh3" class="field-group__label">field-group label</label>
+    <input type="checkbox" id="3gjh3" />
+  </div>
+  <p class="field-group__info">field-group info</p>
+</div>
+
+<!-- boolean with infotext -->
+<div class="field-group field-group_boolean">
+  <label for="3gjh3" class="field-group__label">
+    field-group label
+    <p class="field-group__info">field-group info</p>
+  </label>
+  <input type="checkbox" id="3gjh3" />
 </div>`,
       withGridMarkup: `<div data-grid>
   <div data-col="6">
@@ -151,6 +169,8 @@ export default {
           type: 'map',
           default: '',
         },
+      ],
+      labelParams: [
         {
           name: '$field-group-label-spacing',
           type: 'array',
@@ -166,10 +186,12 @@ export default {
           type: 'map',
           default: '',
         },
+      ],
+      infoParams: [
         {
           name: '$field-group-info-spacing',
           type: 'array',
-          default: '8',
+          default: '8 4',
         },
         {
           name: '$field-group-info-font-size',
